1bb34a14073f7c6226bac80789918167efe0c9e6
OmegaMadPenguin <rahnfelj@informatik.uni-freiburg.de>
2020-08-19 11:43:53 +0200

    apply papers layer loss function instead of l2

--- utils/loss_functions.py
+++ utils/loss_functions.py
@@ -20,13 +20,19 @@ def l2_loss(x_rec, x_true):
 
     return loss
 
+def layer_loss(pred, target):
+    C, H, W = pred.size()[1:]
+    loss = torch.sum((pred - target).pow(2), dim=[1,2,3]) / (2 * C * H * W)
+
+    return loss
+
 def vgg123_loss(x_rec, x_true):
     out1_rec, out2_rec, out3_rec = vgg123(x_rec)
     out1_true, out2_true, out3_true = vgg123(x_true)
 
-    loss1 = torch.sum((out1_rec - out1_true).pow(2), dim=[1,2,3])
-    loss2 = torch.sum((out2_rec - out2_true).pow(2), dim=[1,2,3])
-    loss3 = torch.sum((out3_rec - out3_true).pow(2), dim=[1,2,3])
+    loss1 = layer_loss(out1_rec, out1_true)
+    loss2 = layer_loss(out2_rec, out2_true)
+    loss3 = layer_loss(out3_rec, out3_true)
     loss = torch.mean(loss1 + loss2 + loss3)
 
     return loss
@@ -35,9 +41,9 @@ def vgg345_loss(x_rec, x_true):
     out1_rec, out2_rec, out3_rec = vgg345(x_rec)
     out1_true, out2_true, out3_true = vgg345(x_true)
 
-    loss1 = torch.sum((out1_rec - out1_true).pow(2), dim=[1,2,3])
-    loss2 = torch.sum((out2_rec - out2_true).pow(2), dim=[1,2,3])
-    loss3 = torch.sum((out3_rec - out3_true).pow(2), dim=[1,2,3])
+    loss1 = layer_loss(out1_rec, out1_true)
+    loss2 = layer_loss(out2_rec, out2_true)
+    loss3 = layer_loss(out3_rec, out3_true)
     loss = torch.mean(loss1 + loss2 + loss3)
 
     return loss
