f2ee7ea97cf3ad13952849d60847f44858b7431d
OmegaMadPenguin <rahnfelj@informatik.uni-freiburg.de>
2020-08-20 12:28:04 +0200

    rename: WEIGHT_DECAY -> LEARNING_RATE_DECAY

--- utils/hyperparameters.py
+++ utils/hyperparameters.py
@@ -2,7 +2,7 @@ DIM_LATENT = 100
 EPOCHS = 5
 BATCH_SIZE = 64
 LEARNING_RATE = 0.0005
-WEIGHT_DECAY = 0.5
+LEARNING_RATE_DECAY = 0.5
 ALPHA = 1
 BETA = 0.5
 
@@ -21,7 +21,7 @@ def print_hyperparameters(loss_id, save_epochs):
     print("number of epochs                 : {0}".format(EPOCHS))
     print("batch size                       : {0}".format(BATCH_SIZE))
     print("learning rate                    : {0}".format(LEARNING_RATE))
-    print("weight decay                     : {0}".format(WEIGHT_DECAY))
+    print("weight decay                     : {0}".format(LEARNING_RATE_DECAY))
     print("alpha (kl loss)                  : {0}".format(ALPHA))
     print("beta (rec loss)                  : {0}".format(BETA))
     print("rec loss                         : {0}".format(["l2_loss", "vgg123_loss",\
