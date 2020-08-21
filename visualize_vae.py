534c4eab78acac06409159bc38d65fbd75be92ee
OmegaMadPenguin <rahnfelj@informatik.uni-freiburg.de>
2020-08-21 15:09:52 +0200

    add img show using multiple vae's

--- visualize_vae.py
+++ visualize_vae.py
@@ -12,9 +12,16 @@ from utils.img_transforms import transform, transform_back
 from models.vae import VAE
 
 
+def grid_add_img(img, fig, rows, cols, pos):
+    fig.add_subplot(rows, cols, pos)
+    plt.imshow(img)
+    plt.axis('off')
+
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--vae', type=str, required=True, help="path to vae")
+    parser.add_argument('--vae', type=str, nargs='+', required=True,\
+        help="path to vae")
     parser.add_argument('--imgdir', type=str, required=True,\
         help="path to image folder")
     
@@ -23,35 +30,31 @@ if __name__ == '__main__':
     img_names = os.listdir(args.imgdir)
     n_imgs = len(img_names)
 
-    vae = VAE(DIM_LATENT)
-    vae.load_state_dict(torch.load(args.vae))
+    rows = len(args.vae) + 1
 
     fig = plt.figure()
 
     with torch.no_grad():
-        for i in range(n_imgs):
-            path = os.path.join(args.imgdir, img_names[i])
-            img = Image.open(path)
-            
-            x_true = transform(img)
-            x_true = x_true.unsqueeze(0)
-            x_true = x_true.view(1, 3, 64, 64)
-            x_rec, mu, logvar = vae(x_true)
+        for k in range(len(args.vae)):
+            vae = VAE(DIM_LATENT)
+            vae.load_state_dict(torch.load(args.vae[k]))
+            for i in range(n_imgs):
+                path = os.path.join(args.imgdir, img_names[i])
+                img = Image.open(path)
+                
+                x_true = transform(img)
+                x_true = x_true.unsqueeze(0)
+                x_true = x_true.view(1, 3, 64, 64)
+                x_rec, mu, logvar = vae(x_true)
 
-            x_true = x_true.squeeze(0)
-            x_rec = x_rec.squeeze(0)
+                x_true = x_true.squeeze(0)
+                x_rec = x_rec.squeeze(0)
 
-            # denormalize
-            img_true = transform_back(x_true)
-            img_rec = transform_back(x_rec)
+                img_true = transform_back(x_true)
+                img_rec = transform_back(x_rec)
 
-            fig.add_subplot(2, n_imgs, i+1)
-            plt.imshow(img_true)
-            plt.axis('off')
-
-            fig.add_subplot(2, n_imgs, n_imgs+i+1)
-            plt.imshow(img_rec)
-            plt.axis('off')
+                if k == 0: grid_add_img(img_true, fig, rows, n_imgs, i+1)
+                grid_add_img(img_rec, fig, rows, n_imgs, (k+1)*n_imgs+i+1)
 
     fig.subplots_adjust(wspace=0, hspace=0)
     plt.show()
